6ed4cb92741b311b634d00f9f5bbd7572469e39b
Priyanshu Sidar <97578019+Wannabe-King@users.noreply.github.com>
2024-02-02 23:45:16 +0530

    setting up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
+import 'dart:io';
+
 import 'package:chatapp/pages/chatpage.dart';
 import 'package:chatapp/pages/home.dart';
 import 'package:chatapp/pages/signin.dart';
 import 'package:chatapp/pages/signup.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+          apiKey: 'AIzaSyA1lOiARKUW3K1fcmLJ5Fr2OQPHAAaKpfs',
+          appId: '1:12716894522:android:91782c683ec584593f4803',
+          messagingSenderId: '12716894522',
+          projectId: 'chatapp-7dedc',
+        ))
+      : await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
